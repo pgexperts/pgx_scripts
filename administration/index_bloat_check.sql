23dd6d840a9eef1a448e6c9b6099fea82efa9280
Josh Berkus <josh@pgexperts.com>
2014-10-10 14:00:40 -0700

    added new bloat checks.

---
+++ administration/index_bloat_check.sql
@@ -0,0 +1,83 @@
+WITH btree_index_atts AS (
+    SELECT nspname, relname, reltuples, relpages, indrelid, relam,
+        regexp_split_to_table(indkey::text, ' ')::smallint AS attnum,
+        indexrelid as index_oid
+    FROM pg_index
+    JOIN pg_class ON pg_class.oid=pg_index.indexrelid
+    JOIN pg_namespace ON pg_namespace.oid = pg_class.relnamespace
+    JOIN pg_am ON pg_class.relam = pg_am.oid
+    WHERE pg_am.amname = 'btree'
+    ),
+index_item_sizes AS (
+    SELECT
+    i.nspname, i.relname, i.reltuples, i.relpages, i.relam,
+    s.starelid, a.attrelid AS table_oid, index_oid,
+    current_setting('block_size')::numeric AS bs,
+    /* MAXALIGN: 4 on 32bits, 8 on 64bits (and mingw32 ?) */
+    CASE
+        WHEN version() ~ 'mingw32' OR version() ~ '64-bit' THEN 8
+        ELSE 4
+    END AS maxalign,
+    24 AS pagehdr,
+    /* per tuple header: add index_attribute_bm if some cols are null-able */
+    CASE WHEN max(coalesce(s.stanullfrac,0)) = 0
+        THEN 2
+        ELSE 6
+    END AS index_tuple_hdr,
+    /* data len: we remove null values save space using it fractionnal part from stats */
+    sum( (1-coalesce(s.stanullfrac, 0)) * coalesce(s.stawidth, 2048) ) AS nulldatawidth
+    FROM pg_attribute AS a
+    JOIN pg_statistic AS s ON s.starelid=a.attrelid AND s.staattnum = a.attnum
+    JOIN btree_index_atts AS i ON i.indrelid = a.attrelid AND a.attnum = i.attnum
+    WHERE a.attnum > 0
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
+),
+index_aligned AS (
+    SELECT maxalign, bs, nspname, relname AS index_name, reltuples,
+        relpages, relam, table_oid, index_oid,
+      ( 2 +
+          maxalign - CASE /* Add padding to the index tuple header to align on MAXALIGN */
+            WHEN index_tuple_hdr%maxalign = 0 THEN maxalign
+            ELSE index_tuple_hdr%maxalign
+          END
+        + nulldatawidth + maxalign - CASE /* Add padding to the data to align on MAXALIGN */
+            WHEN nulldatawidth::integer%maxalign = 0 THEN maxalign
+            ELSE nulldatawidth::integer%maxalign
+          END
+      )::numeric AS nulldatahdrwidth, pagehdr
+    FROM index_item_sizes AS s1
+),
+otta_calc AS (
+  SELECT bs, nspname, table_oid, index_oid, index_name, relpages, coalesce(
+    ceil((reltuples*(4+nulldatahdrwidth))/(bs-pagehdr::float)) +
+      CASE WHEN am.amname IN ('hash','btree') THEN 1 ELSE 0 END , 0 -- btree and hash have a metadata reserved block
+    ) AS otta
+  FROM index_aligned AS s2
+    LEFT JOIN pg_am am ON s2.relam = am.oid
+),
+raw_bloat AS (
+    SELECT current_database() as dbname, nspname, c.relname AS table_name, index_name,
+        bs*(sub.relpages)::bigint AS totalbytes,
+        CASE
+            WHEN sub.relpages <= otta THEN 0
+            ELSE bs*(sub.relpages-otta)::bigint END
+            AS wastedbytes,
+        CASE
+            WHEN sub.relpages <= otta
+            THEN 0 ELSE bs*(sub.relpages-otta)::bigint * 100 / (bs*(sub.relpages)::bigint) END
+            AS realbloat,
+        pg_relation_size(sub.table_oid) as table_bytes,
+        stat.idx_scan as index_scans
+    FROM otta_calc AS sub
+    JOIN pg_class AS c ON c.oid=sub.table_oid
+    JOIN pg_stat_user_indexes AS stat ON sub.index_oid = stat.indexrelid
+)
+SELECT dbname as database_name, nspname as schema_name, table_name, index_name,
+        round(realbloat, 1) as bloat_pct,
+        wastedbytes as bloat_bytes, pg_size_pretty(wastedbytes::bigint) as bloat_size,
+        totalbytes as index_bytes, pg_size_pretty(totalbytes::bigint) as index_size,
+        table_bytes, pg_size_pretty(table_bytes) as table_size,
+        index_scans
+FROM raw_bloat
+WHERE ( realbloat > 50 and wastedbytes > 50000000 )
+ORDER BY wastedbytes DESC;
---
+++ administration/table_bloat_check.sql
@@ -0,0 +1,125 @@
+-- new table bloat query
+-- still needs work; is often off by +/- 20%
+WITH constants AS (
+    -- define some constants for sizes of things
+    -- for reference down the query and easy maintenance
+    SELECT current_setting('block_size')::numeric AS bs, 23 AS hdr, 8 AS ma
+),
+no_stats AS (
+    -- screen out table who have attributes
+    -- which dont have stats, such as JSON
+    SELECT table_schema, table_name
+    FROM information_schema.columns
+        LEFT OUTER JOIN pg_stats
+        ON table_schema = schemaname
+            AND table_name = tablename
+            AND column_name = attname
+    WHERE attname IS NULL
+        AND table_schema NOT IN ('pg_catalog', 'information_schema')
+    GROUP BY table_schema, table_name
+),
+null_headers AS (
+    -- calculate null header sizes
+    -- omitting tables which dont have complete stats
+    -- and attributes which aren't visible
+    SELECT
+        hdr+1+(sum(case when null_frac <> 0 THEN 1 else 0 END)/8) as nullhdr,
+        SUM((1-null_frac)*avg_width) as datawidth,
+        MAX(null_frac) as maxfracsum,
+        schemaname,
+        tablename,
+        hdr, ma, bs
+    FROM pg_stats CROSS JOIN constants
+        LEFT OUTER JOIN no_stats
+            ON schemaname = no_stats.table_schema
+            AND tablename = no_stats.table_name
+    WHERE schemaname NOT IN ('pg_catalog', 'information_schema')
+        AND no_stats.table_name IS NULL
+        AND EXISTS ( SELECT 1
+            FROM information_schema.columns
+                WHERE schemaname = columns.table_schema
+                    AND tablename = columns.table_name )
+    GROUP BY schemaname, tablename, hdr, ma, bs
+),
+data_headers AS (
+    -- estimate header and row size
+    SELECT
+        ma, bs, hdr, schemaname, tablename,
+        (datawidth+(hdr+ma-(case when hdr%ma=0 THEN ma ELSE hdr%ma END)))::numeric AS datahdr,
+        (maxfracsum*(nullhdr+ma-(case when nullhdr%ma=0 THEN ma ELSE nullhdr%ma END))) AS nullhdr2
+    FROM null_headers
+),
+table_estimates AS (
+    -- make estimates of how large the table should be
+    -- based on row and page size
+    SELECT schemaname, tablename, bs,
+        reltuples, relpages * bs as table_bytes,
+    CEIL((reltuples*
+            (datahdr + nullhdr2 + 4 + ma -
+                (CASE WHEN datahdr%ma=0
+                    THEN ma ELSE datahdr%ma END)
+                )/(bs-20))) * bs AS expected_bytes
+    FROM data_headers
+        JOIN pg_class ON tablename = relname
+        JOIN pg_namespace ON relnamespace = pg_namespace.oid
+            AND schemaname = nspname
+    WHERE pg_class.relkind = 'r'
+),
+table_estimates_plus AS (
+-- add some extra metadata to the table data
+-- and calculations to be reused
+-- including whether we cant estimate it
+-- or whether we think it might be compressed
+    SELECT current_database() as databasename,
+            schemaname, tablename, reltuples as est_rows,
+            CASE WHEN expected_bytes > 0 AND table_bytes > 0 THEN
+                TRUE ELSE FALSE END as can_estimate,
+            CASE WHEN expected_bytes > table_bytes THEN
+                TRUE ELSE FALSE END as is_compressed,
+            CASE WHEN table_bytes > 0
+                THEN table_bytes::NUMERIC
+                ELSE NULL::NUMERIC END
+                AS table_bytes,
+            CASE WHEN expected_bytes > 0 
+                THEN expected_bytes::NUMERIC
+                ELSE NULL::NUMERIC END
+                    AS expected_bytes,
+            CASE WHEN expected_bytes > 0 AND table_bytes > 0
+                AND expected_bytes <= table_bytes
+                THEN (table_bytes - expected_bytes)::NUMERIC
+                ELSE 0::NUMERIC END AS bloat_bytes
+    FROM table_estimates
+),
+bloat_data AS (
+    -- do final math calculations and formatting
+    select current_database() as databasename,
+        schemaname, tablename, can_estimate, is_compressed,
+        table_bytes, pg_size_pretty(table_bytes) as table_size,
+        expected_bytes, pg_size_pretty(expected_bytes) as expected_size,
+        round(bloat_bytes*100/table_bytes) as pct_bloat,
+        round(bloat_bytes/(1024::NUMERIC^2),2) as mb_bloat,
+        table_bytes, expected_bytes
+    FROM table_estimates_plus
+)
+-- filter output for bloated tables
+SELECT databasename, schemaname, tablename,
+    can_estimate, is_compressed,
+    pct_bloat, mb_bloat,
+    table_size, expected_size
+FROM bloat_data
+-- this where clause defines which tables actually appear
+-- in the bloat chart
+-- example below filters for tables which are either 50%
+-- bloated and more than 20mb in size, or more than 25%
+-- bloated and more than 4GB in size
+WHERE ( pct_bloat >= 50 AND mb_bloat >= 10 )
+    OR ( pct_bloat >= 25 AND mb_bloat >= 1000 )
+ORDER BY pct_bloat DESC;
+
+
+
+
+
+
+
+
