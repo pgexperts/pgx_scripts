32a530ab1a25d59114b063506ea4194f608e83ba
Josh Berkus <josh@pgexperts.com>
2014-10-28 10:45:45 -0700

    draft fk-index checker.  Not ready for prime time yet.

---
+++ indexes/duplicate_indexes_fuzzy.sql
@@ -0,0 +1,42 @@
+-- check for containment
+-- i.e. index A contains index B
+-- and both share the same first column
+-- but they are NOT identical
+
+WITH index_cols_ord as (
+    SELECT attrelid, attnum, attname
+    FROM pg_attribute
+        JOIN pg_index ON indexrelid = attrelid
+    WHERE indkey[0] > 0
+    ORDER BY attrelid, attnum
+),
+index_col_list AS (
+    SELECT attrelid,
+        array_agg(attname) as cols
+    FROM index_cols_ord
+    GROUP BY attrelid
+),
+dup_natts AS (
+SELECT indrelid, indexrelid
+FROM pg_index as ind
+WHERE EXISTS ( SELECT 1
+    FROM pg_index as ind2
+    WHERE ind.indrelid = ind2.indrelid
+    AND ( ind.indkey @> ind2.indkey
+     OR ind.indkey <@ ind2.indkey )
+    AND ind.indkey[0] = ind2.indkey[0]
+    AND ind.indkey <> ind2.indkey
+    AND ind.indexrelid <> ind2.indexrelid
+) )
+SELECT userdex.schemaname as schema_name,
+    userdex.relname as table_name,
+    userdex.indexrelname as index_name,
+    array_to_string(cols, ', ') as index_cols,
+    indexdef,
+    idx_scan as index_scans
+FROM pg_stat_user_indexes as userdex
+    JOIN index_col_list ON index_col_list.attrelid = userdex.indexrelid
+    JOIN dup_natts ON userdex.indexrelid = dup_natts.indexrelid
+    JOIN pg_indexes ON userdex.schemaname = pg_indexes.schemaname
+        AND userdex.indexrelname = pg_indexes.indexname
+ORDER BY userdex.schemaname, userdex.relname, cols, userdex.indexrelname;
---
+++ indexes/fk_no_index.sql
@@ -0,0 +1,94 @@
+-- check for FKs where there is no matching index
+-- on the referencing side
+-- or a bad index
+
+WITH fk_actions ( code, action ) AS (
+    VALUES ( 'a', 'error' ),
+        ( 'r', 'restrict' ),
+        ( 'c', 'cascade' ),
+        ( 'n', 'set null' ),
+        ( 'd', 'set default' )
+),
+fk_list AS (
+    SELECT pg_constraint.oid as fkoid, conrelid, confrelid as referrelid,
+        conname, relname, nspname,
+        fk_actions_update.action as update_action,
+        fk_actions_delete.action as delete_action,
+        conkey as key_cols
+    FROM pg_constraint
+        JOIN pg_class ON conrelid = pg_class.oid
+        JOIN pg_namespace ON pg_class.relnamespace = pg_namespace.oid
+        JOIN fk_actions AS fk_actions_update ON confupdtype = fk_actions_update.code
+        JOIN fk_actions AS fk_actions_delete ON confdeltype = fk_actions_delete.code
+    WHERE contype = 'f'
+),
+fk_attributes AS (
+    SELECT fkoid, conrelid, attname, attnum
+    FROM fk_list
+        JOIN pg_attribute
+            ON conrelid = attrelid
+            AND attnum = ANY( key_cols )
+    ORDER BY fkoid, attnum
+),
+fk_cols_list AS (
+    SELECT fkoid, array_agg(attname) as cols_list
+    FROM fk_attributes
+    GROUP BY fkoid
+),
+index_list AS (
+    SELECT indexrelid as indexid,
+        pg_class.relname as indexname,
+        indrelid,
+        indkey,
+        indpred is not null as has_predicate,
+        pg_get_indexdef(indexrelid) as indexdef
+    FROM pg_index
+        JOIN pg_class ON indexrelid = pg_class.oid
+    WHERE indisvalid
+),
+fk_index_match AS (
+    SELECT fk_list.*,
+        indexid,
+        indexname,
+        indkey::int[] as indexatts,
+        has_predicate,
+        indexdef,
+        array_upper(key_cols, 1) as fk_colcount,
+        array_upper(indkey,1) as index_colcount,
+        round(pg_relation_size(conrelid)/(1024^2)::numeric) as table_mb,
+        cols_list
+    FROM fk_list
+        JOIN fk_cols_list USING (fkoid)
+        LEFT OUTER JOIN index_list
+            ON conrelid = indrelid
+            AND (indkey::int2[])[0:(array_upper(key_cols,1) -1)] @> key_cols
+),
+fk_perfect_match AS (
+    SELECT fkoid
+    FROM fk_index_match
+    WHERE (index_colcount - 1) <= fk_colcount
+        AND NOT has_predicate
+        AND indexdef LIKE '%USING btree%'
+),
+fk_index_check AS (
+    SELECT 'no index' as issue, *, 1 as issue_sort
+    FROM fk_index_match
+    WHERE indexid IS NULL
+    UNION ALL
+    SELECT 'questionable index' as issue, *, 2
+    FROM fk_index_match
+    WHERE indexid IS NOT NULL
+        AND fkoid NOT IN (
+            SELECT fkoid
+            FROM fk_perfect_match)
+)
+SELECT nspname as schema_name,
+    relname as table_name,
+    conname as fk_name,
+    issue,
+    table_mb,
+    cols_list,
+    indexdef
+FROM fk_index_check
+--WHERE table_mb > 9
+ORDER BY issue_sort, table_mb DESC, fk_name;
