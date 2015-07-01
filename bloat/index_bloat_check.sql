61923e18904560cff205aa90d747d3c985c0d887
Josh Berkus <josh@agliodbs.com>
2015-06-30 17:24:13 -0700

    Oops, pushed wrong version of index_bloat.  This is the correct version, with non-superuser access.

--- bloat/index_bloat_check.sql
+++ bloat/index_bloat_check.sql
@@ -1,73 +1,88 @@
+-- btree index stats query
+-- estimates bloat for btree indexes
 WITH btree_index_atts AS (
-    SELECT nspname, relname, reltuples, relpages, indrelid, relam,
+    SELECT nspname, 
+        indexclass.relname as index_name, 
+        indexclass.reltuples, 
+        indexclass.relpages, 
+        indrelid, indexrelid,
+        indexclass.relam,
+        tableclass.relname as tablename,
         regexp_split_to_table(indkey::text, ' ')::smallint AS attnum,
         indexrelid as index_oid
     FROM pg_index
-    JOIN pg_class ON pg_class.oid=pg_index.indexrelid
-    JOIN pg_namespace ON pg_namespace.oid = pg_class.relnamespace
-    JOIN pg_am ON pg_class.relam = pg_am.oid
-    WHERE pg_am.amname = 'btree'
-        AND nspname NOT IN ('pg_catalog','information_schema')
+    JOIN pg_class AS indexclass ON pg_index.indexrelid = indexclass.oid
+    JOIN pg_class AS tableclass ON pg_index.indrelid = tableclass.oid
+    JOIN pg_namespace ON pg_namespace.oid = indexclass.relnamespace
+    JOIN pg_am ON indexclass.relam = pg_am.oid
+    WHERE pg_am.amname = 'btree' and indexclass.relpages > 0
+         --AND nspname NOT IN ('pg_catalog','information_schema')
     ),
 index_item_sizes AS (
     SELECT
-    i.nspname, i.relname, i.reltuples, i.relpages, i.relam,
-    s.starelid, a.attrelid AS table_oid, index_oid,
+    ind_atts.nspname, ind_atts.index_name, 
+    ind_atts.reltuples, ind_atts.relpages, ind_atts.relam,
+    indrelid AS table_oid, index_oid,
     current_setting('block_size')::numeric AS bs,
     8 AS maxalign,
     24 AS pagehdr,
-    /* per tuple header: add index_attribute_bm if some cols are null-able */
-    CASE WHEN max(coalesce(s.stanullfrac,0)) = 0
+    CASE WHEN max(coalesce(pg_stats.null_frac,0)) = 0
         THEN 2
         ELSE 6
     END AS index_tuple_hdr,
-    /* data len: we remove null values save space using it fractionnal part from stats */
-    sum( (1-coalesce(s.stanullfrac, 0)) * coalesce(s.stawidth, 2048) ) AS nulldatawidth
-    FROM pg_attribute AS a
-    JOIN pg_statistic AS s ON s.starelid=a.attrelid AND s.staattnum = a.attnum
-    JOIN btree_index_atts AS i ON i.indrelid = a.attrelid AND a.attnum = i.attnum
-    WHERE a.attnum > 0
+    sum( (1-coalesce(pg_stats.null_frac, 0)) * coalesce(pg_stats.avg_width, 1024) ) AS nulldatawidth
+    FROM pg_attribute
+    JOIN btree_index_atts AS ind_atts ON pg_attribute.attrelid = ind_atts.indexrelid AND pg_attribute.attnum = ind_atts.attnum
+    JOIN pg_stats ON pg_stats.schemaname = ind_atts.nspname
+          -- stats for regular index columns
+          AND ( (pg_stats.tablename = ind_atts.tablename AND pg_stats.attname = pg_catalog.pg_get_indexdef(pg_attribute.attrelid, pg_attribute.attnum, TRUE)) 
+          -- stats for functional indexes
+          OR   (pg_stats.tablename = ind_atts.index_name AND pg_stats.attname = pg_attribute.attname))
+    WHERE pg_attribute.attnum > 0
     GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
 ),
-index_aligned AS (
-    SELECT maxalign, bs, nspname, relname AS index_name, reltuples,
+index_aligned_est AS (
+    SELECT maxalign, bs, nspname, index_name, reltuples,
         relpages, relam, table_oid, index_oid,
-      ( 2 +
-          maxalign - CASE /* Add padding to the index tuple header to align on MAXALIGN */
-            WHEN index_tuple_hdr%maxalign = 0 THEN maxalign
-            ELSE index_tuple_hdr%maxalign
-          END
-        + nulldatawidth + maxalign - CASE /* Add padding to the data to align on MAXALIGN */
-            WHEN nulldatawidth::integer%maxalign = 0 THEN maxalign
-            ELSE nulldatawidth::integer%maxalign
-          END
-      )::numeric AS nulldatahdrwidth, pagehdr
-    FROM index_item_sizes AS s1
-),
-otta_calc AS (
-  SELECT bs, nspname, table_oid, index_oid, index_name, relpages, coalesce(
-    ceil((reltuples*(4+nulldatahdrwidth))/(bs-pagehdr::float)) +
-      CASE WHEN am.amname IN ('hash','btree') THEN 1 ELSE 0 END , 0 -- btree and hash have a metadata reserved block
-    ) AS otta
-  FROM index_aligned AS s2
-    LEFT JOIN pg_am am ON s2.relam = am.oid
+        coalesce (
+            ceil (
+                reltuples * ( 6 
+                    + maxalign 
+                    - CASE
+                        WHEN index_tuple_hdr%maxalign = 0 THEN maxalign
+                        ELSE index_tuple_hdr%maxalign
+                      END
+                    + nulldatawidth 
+                    + maxalign 
+                    - CASE /* Add padding to the data to align on MAXALIGN */
+                        WHEN nulldatawidth::integer%maxalign = 0 THEN maxalign
+                        ELSE nulldatawidth::integer%maxalign
+                      END
+                )::numeric 
+              / ( bs - pagehdr::NUMERIC )
+              +1 )
+         , 0 )
+      as expected
+    FROM index_item_sizes
 ),
 raw_bloat AS (
-    SELECT current_database() as dbname, nspname, c.relname AS table_name, index_name,
-        bs*(sub.relpages)::bigint AS totalbytes, otta as expected,
+    SELECT current_database() as dbname, nspname, pg_class.relname AS table_name, index_name,
+        bs*(index_aligned_est.relpages)::bigint AS totalbytes, expected,
         CASE
-            WHEN sub.relpages <= otta THEN 0
-            ELSE bs*(sub.relpages-otta)::bigint END
-            AS wastedbytes,
+            WHEN index_aligned_est.relpages <= expected 
+                THEN 0
+                ELSE bs*(index_aligned_est.relpages-expected)::bigint 
+            END AS wastedbytes,
         CASE
-            WHEN sub.relpages <= otta
-            THEN 0 ELSE bs*(sub.relpages-otta)::bigint * 100 / (bs*(sub.relpages)::bigint) END
-            AS realbloat,
-        pg_relation_size(sub.table_oid) as table_bytes,
+            WHEN index_aligned_est.relpages <= expected
+                THEN 0 
+                ELSE bs*(index_aligned_est.relpages-expected)::bigint * 100 / (bs*(index_aligned_est.relpages)::bigint) 
+            END AS realbloat,
+        pg_relation_size(index_aligned_est.table_oid) as table_bytes,
         stat.idx_scan as index_scans
-    FROM otta_calc AS sub
-    JOIN pg_class AS c ON c.oid=sub.table_oid
-    JOIN pg_stat_user_indexes AS stat ON sub.index_oid = stat.indexrelid
+    FROM index_aligned_est
+    JOIN pg_class ON pg_class.oid=index_aligned_est.table_oid
+    JOIN pg_stat_user_indexes AS stat ON index_aligned_est.index_oid = stat.indexrelid
 ),
 format_bloat AS (
 SELECT dbname as database_name, nspname as schema_name, table_name, index_name,
