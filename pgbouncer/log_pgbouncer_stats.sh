bfbbbc81385197c9f8a76f74b08c741fe52a7837
Josh Berkus <josh@agliodbs.com>
2015-10-19 10:51:10 -0700

    Added script to log pgbouncer stats.

---
+++ pgbouncer/log_pgbouncer_stats.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# take number of minutes as parameter; otherwise
+# run for 1 hour
+NUMMIN=${1:-60}
+NUMMIN=$(($NUMMIN * 2 + 10))
+CURMIN=0
+
+export PGUSER=pgbouncer
+export PGPASSWORD=
+export PGHOST=127.0.0.1
+export PGDATABASE=pgbouncer
+
+adddate() {
+    while IFS= read -r line; do
+        echo "$(date +"%Y-%m-%d %H:%M:%S")|$line"
+    done
+}
+
+while [ CURMIN -lt NUMMIN  ] 
+do
+
+    psql -q -A -t -c "show pools" | adddate >> pools.log
+    
+    psql -q -A -t -c "show stats" | adddate >> stats.log
+    
+    psql -q -A -t -c "show clients" | adddate >> clients.log
+
+    #cycle twice per minute
+    sleep 30
+    let CURMIN=CURMIN+1 
+ 
+done
+
+exit 0
