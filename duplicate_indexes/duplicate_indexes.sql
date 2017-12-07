3b874bfda848338ae8043088c2ebbbbeab5fad8f
Christophe Pettus <cpettus@pgexperts.com>
2017-12-07 11:55:56 -0800

    Fix bug involving attribute indexes that were prefixes of each other.

--- duplicate_indexes/duplicate_indexes.sql
+++ duplicate_indexes/duplicate_indexes.sql
@@ -3,17 +3,17 @@ SELECT ni.nspname || '.' || ct.relname AS "table",
        pg_get_indexdef(i.indexrelid) AS "dup index definition", 
        i.indkey AS "dup index attributes",
        cii.relname AS "encompassing index", 
-       pg_get_indexdef(ii.indexrelid) AS "encompassing index definition"
-       ii.indkey AS "enc index attributes",
+       pg_get_indexdef(ii.indexrelid) AS "encompassing index definition",
+       ii.indkey AS "enc index attributes"
   FROM pg_index i
   JOIN pg_class ct ON i.indrelid=ct.oid
   JOIN pg_class ci ON i.indexrelid=ci.oid
   JOIN pg_namespace ni ON ci.relnamespace=ni.oid
   JOIN pg_index ii ON ii.indrelid=i.indrelid AND
                       ii.indexrelid != i.indexrelid AND
-                      array_to_string(ii.indkey, ' ') like (array_to_string(i.indkey, ' ') || '%') AND
-                      array_to_string(ii.indcollation, ' ') like (array_to_string(i.indcollation, ' ') || '%') AND
-                      array_to_string(ii.indclass, ' ') like (array_to_string(i.indclass, ' ') || '%') AND
+                      (array_to_string(ii.indkey, ' ') || ' ') like (array_to_string(i.indkey, ' ') || ' %') AND
+                      (array_to_string(ii.indcollation, ' ')  || ' ') like (array_to_string(i.indcollation, ' ') || ' %') AND
+                      (array_to_string(ii.indclass, ' ')  || ' ') like (array_to_string(i.indclass, ' ') || ' %') AND
                       NOT (ii.indkey::integer[] @> ARRAY[0]) AND
                       NOT (i.indkey::integer[] @> ARRAY[0]) AND
                       CASE WHEN i.indisunique THEN ii.indisunique ELSE true END
