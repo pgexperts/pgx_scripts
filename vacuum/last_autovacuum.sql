53c01a2cd4b09ebfff45962857ced5fab6d9f3b5
Jeff Frost <jeff.frost@procore.com>
2021-09-23 13:30:01 -0500

    Style and capitalization support update
    Support mixed case table/schema names as well as COALESCE
    the most recent of last_analyze and last_autoanalyze to
    give a more useful view of the status.

--- vacuum/last_autovacuum.sql
+++ vacuum/last_autovacuum.sql
@@ -1,18 +1,23 @@
--- Show last autovacuum by table size descending.
-select
-    pg_class.relname,
-    pg_namespace.nspname,
-    pg_size_pretty(pg_total_relation_size(pg_namespace.nspname::text || '.' || pg_class.relname::text)),
-    pg_stat_all_tables.last_autovacuum,
-    pg_relation_size(pg_namespace.nspname::text || '.' || pg_class.relname::text)
-from
-    pg_class
-        join pg_namespace
-            on pg_class.relnamespace = pg_namespace.oid
-        join pg_stat_all_tables
-            on (pg_class.relname = pg_stat_all_tables.relname AND pg_namespace.nspname = pg_stat_all_tables.schemaname)
-where
-    pg_namespace.nspname not in ('pg_toast')
-order by
-    5 desc
+-- Show last vacuum or autovacuum by table size descending.
+SELECT pg_class.relname
+     , pg_namespace.nspname
+     , pg_size_pretty(pg_total_relation_size(pg_class.oid))
+     , 
+       CASE
+        WHEN COALESCE(last_vacuum,'1/1/1000') >
+         COALESCE(last_autovacuum,'1/1/1000') THEN
+         pg_stat_all_tables.last_vacuum
+       ELSE last_autovacuum
+       END AS last_vacuumed
+     , pg_relation_size(pg_class.oid)
+  FROM pg_class
+  JOIN pg_namespace
+    ON pg_class.relnamespace                 = pg_namespace.oid
+  JOIN pg_stat_all_tables
+    ON (
+        pg_class.relname                      = pg_stat_all_tables.relname
+   AND pg_namespace.nspname                  = pg_stat_all_tables.schemaname
+       )
+ WHERE pg_namespace.nspname not in ('pg_toast')
+ ORDER BY pg_relation_size(pg_class.oid) DESC
 ;
