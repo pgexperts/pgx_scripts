6db52c2219af460d4739a2b481ab215a7906dc49
Quinn Weaver <quinn.weaver@gmail.com>
2023-08-07 19:30:17 -0700

    Port integer_usage.py to Python 3 and add a --debug option

--- columns/integer_usage.py
+++ columns/integer_usage.py
@@ -1,13 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import psycopg2
 import sys
 from optparse import OptionParser
 from time import sleep
+import os
 
 parser = OptionParser()
 parser.add_option("-d", "--database", dest="dbname",
                   help="database to scanned (required)", metavar="DBNAME")
+parser.add_option("--debug", action="store_true", default=False, dest="debug",
+                  help="print verbose debugging information", metavar="DEBUG")
 parser.add_option("-m", "--minimum", dest="minimum",
                     help="minimum percent used", metavar="PERCENT",
                     default="0")
@@ -29,11 +32,15 @@ parser.add_option("-w", "--password", dest="dbpass",
 def dq(val):
     return '"' + val + '"'
 
+if options.debug:
+    for var_name in ("PGHOST", "PGUSER", "PGPORT", "PGDATABASE", "PGPASSWORD"):
+        value = os.getenv(var_name)
+        print(f'{var_name}="{value}"')
+
+connect_string = ""
+
 if options.dbname:
     connect_string ="dbname=%s " % options.dbname
-else:
-    print "ERROR: a target database is required."
-    sys.exit(-1)
 
 if options.dbhost:
     connect_string += " host=%s " % options.dbhost
@@ -47,7 +54,7 @@ if options.dbpass:
 if options.dbport:
     connect_string += " port=%s " % options.dbport
 
-conn = psycopg2.connect( connect_string )
+conn = psycopg2.connect(connect_string)
 cur = conn.cursor()
 
 # get a list of all integer fields with a key constraint on them
@@ -66,26 +73,28 @@ cur.execute("""SELECT table_schema, table_name, column_name, data_type,
     WHERE column_default ILIKE '%nextval%'
     ORDER BY table_schema, table_name, column_name""")
 collist = cur.fetchall()
+if options.debug:
+    print(f"Columns:\n\n{collist}")
 
-retval = 0
+exit_status = 0
 rest = False
 if len(collist) > 25:
     rest = True
 # for each, fetch the current maximum value
 for col in collist:
+    (namespace, table, column, data_type, limit) = col
 
     if rest:
         sleep(.2)
 
-    cur.execute("""SELECT max("%s") FROM "%s"."%s" """ % (col[2], col[0], col[1],))
-    maxval = cur.fetchone()
+    cur.execute(f'SELECT max("{column}") FROM "{namespace}"."{table}"')
+    current = cur.fetchone()[0]
 
-    if maxval[0] and col[4]:
+    if current and limit:
+        pctused = current*100.0/float(limit)
 
-        pctused = (maxval[0]*100.0)/float(col[4])
+        if pctused >= float(options.minimum):
+            print(f"Column '{column}' of table '{namespace}.{table}' is {pctused:{3.1}} used")
+            exit_status = 1
 
-        if  pctused >= float(options.minimum):
-            print "Column '%s' Table '%s.%s' is %3.1f%% used" % (col[2],col[0],col[1],pctused,)
-            retval = 1
-
-sys.exit(retval)
+sys.exit(exit_status)
