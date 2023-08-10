64062b9227ec18e247d0b589b8bee2855ced4645
Quinn Weaver <quinn.weaver@gmail.com>
2023-08-10 14:52:33 -0700

    Decimal points: improve rounding and display
    * Use explcit Decimal division
    
    * Also add a --precision option (not honored yet)
    
    TODO: figure out why print() isn't right-justifying my percentages
          as requested

--- columns/integer_usage.py
+++ columns/integer_usage.py
@@ -5,13 +5,15 @@ import sys
 from optparse import OptionParser
 from time import sleep
 import os
+from decimal import *
+
 
 parser = OptionParser()
 parser.add_option("-d", "--database", dest="dbname",
                   help="database to scanned (required)", metavar="DBNAME")
 parser.add_option("--debug", action="store_true", default=False, dest="debug",
                   help="print verbose debugging information", metavar="DEBUG")
-parser.add_option("-m", "--minimum", dest="minimum",
+parser.add_option("-m", "--minimum", dest="minimum", type="int",
                     help="minimum percent used", metavar="PERCENT",
                     default="0")
 parser.add_option("-U", "--user", dest="dbuser",
@@ -26,6 +28,9 @@ parser.add_option("-p", "--port", dest="dbport",
 parser.add_option("-w", "--password", dest="dbpass",
                   help="database password", metavar="PWD",
                   default="")
+parser.add_option("--precision", dest="precision", type="int",
+                  help="output percentage to this many decimal places", metavar="PWD",
+                  default=0)
 
 (options, args) = parser.parse_args()
 
@@ -72,29 +77,44 @@ cur.execute("""SELECT table_schema, table_name, column_name, data_type,
     FROM information_schema.columns
     WHERE column_default ILIKE '%nextval%'
     ORDER BY table_schema, table_name, column_name""")
+
 collist = cur.fetchall()
+if len(collist) > 25:
+    pause = True
+else:
+    pause = False
+
 if options.debug:
     print(f"Columns:\n\n{collist}")
 
 exit_status = 0
-rest = False
-if len(collist) > 25:
-    rest = True
-# for each, fetch the current maximum value
 for col in collist:
     (namespace, table, column, data_type, limit) = col
 
-    if rest:
+    if pause:
         sleep(.2)
 
     cur.execute(f'SELECT max("{column}") FROM "{namespace}"."{table}"')
-    current = cur.fetchone()[0]
+    current_value = cur.fetchone()[0]
 
-    if current and limit:
-        pctused = current*100.0/float(limit)
+    if current_value and limit:
+        precision = 1
+        pctused = Decimal(100.0) * Decimal(current_value) / Decimal(limit)
+        rounded_pct = round(pctused, precision)
 
-        if pctused >= float(options.minimum):
-            print(f"Column '{column}' of table '{namespace}.{table}' is {pctused:{3.1}} used")
+        if pctused >= options.minimum:
             exit_status = 1
+            # print("Column '%s' Table '%s.%s' is %3.1f%% used" % (col[2],col[0],col[1],pctused,))
+            print("{pct}% used: column {column} of table {namespace}.{table}".format(
+                pct = rounded_pct,
+                column = column,
+                namespace = namespace,
+                table = table,
+
+                type = "d",
+                width = 3,
+                precision = precision,
+                align = ">",
+            ))
 
 sys.exit(exit_status)
